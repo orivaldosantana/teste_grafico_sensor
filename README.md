fb05635971c26656aa1f268ee1b14904c53978eb
Orivaldo Santana Jr <orivaldo@gmail.com>
2023-11-19 21:58:38 -0300

    rascunho do gráfico de temperatura

--- README.md
+++ README.md
@@ -1,15 +1,43 @@
 # Teste gráfico de sensor
+
 Teste para gerar gráficos de dados capturados a partir de sensores obtidos em um API
 
-## Aplicação 
+## Aplicação
 
-O código base foi craido com 
+O código base foi craido com
+
 ```
 npx create-react-app teste_grafico_sensor
 ```
 
-Para começar o desenvolvimento do projeto 
+Para começar o desenvolvimento do projeto
+
 ```
 cd teste_grafico_sensor
 npm start
 ```
+
+## Instalação de pacotes
+
+Acesso a uma API:
+
+```
+npm install axios
+```
+
+Gerar gráficos:
+
+```
+npm install recharts
+```
+
+Para usar funções estatísticas, como a mediana:
+
+```
+npm install simple-statistics
+```
+
+# Referências
+
+- [Recharts](https://recharts.org/en-US)
+- [Create a New React App](https://legacy.reactjs.org/docs/create-a-new-react-app.html)
--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import React, { useEffect, useState } from 'react'
+import FrequencyChart from './components/FrequencyChart'
+import axios from 'axios'
+import ss from 'simple-statistics'
+
+const API_URL = 'http://localhost:3002'
 
 function App() {
+  const [frenquencyData, setFrequencyData] = useState([])
+  const [startTime, setStartTime] = useState('03:00:00')
+  const [endTime, setEndTime] = useState('12:00:00')
+  const [startDate, setStartDate] = useState('2023-11-17')
+  const [endDate, setEndDate] = useState('2023-11-17')
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          `${API_URL}/acessos?sensorId=1&dataInicio=${startDate}&dataFim=${endDate}&horaInicio=${startTime}&horaFim=${endTime}`
+        )
+        const dataTemp = []
+        // Código de rascunho para a pegar a mediana da temperatura em cada hora. Teste realizado para horas de 3 a 12
+        for (let h = 3; h < 12; h++) {
+          function filterDate(d) {
+            let dd = new Date(d.createdAt)
+
+            if (dd.getHours() === h) return true
+            else return false
+          }
+          const fff = response.data.filter(filterDate)
+          const tempMedian = ss.median(fff.map(x => x.value))
+          console.log(tempMedian)
+          dataTemp.push({ name: h, temp: tempMedian })
+        }
+        setFrequencyData(dataTemp)
+      } catch (error) {
+        console.error('Erro ao buscar dados da API:', error)
+      }
+    }
+    fetchData()
+  }, [startDate, endDate, startTime, endTime])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Gráfico de frequência</h1>
+      <FrequencyChart data={frenquencyData} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
